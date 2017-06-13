01afa2dfea6948cedd517a08d0651c4b1c749d9c
戴均民 <taichunmin@gmail.com>
2017-06-13 11:20:10 +0800

    Update vue-chosen.js

--- test/vue2-jquery-chosen/vue-chosen.js
+++ test/vue2-jquery-chosen/vue-chosen.js
@@ -1,58 +1,98 @@
+/* global Vue, $ */
 Vue.component('select-chosen', {
-  template: '<select :value="value" v-on:input="updateValue($event.target.value)" :data-placeholder="placeholder" :class="class1 || {}"><option v-for="(name, id) in options" :value="id">{{name}}</option></select>',
+  template:
+    '<select @input="emitValue" :data-placeholder="placeholder" :class="class1 || {}" :multiple="multiple">' +
+    '<option v-for="(name, id) in normalOptions" :value="id">{{name}}</option>' +
+    '<optgroup :label="label" v-for="(groupOptions, label) in optgroups">' +
+    '<option v-for="(name, id) in groupOptions" :value="id">{{name}}</option>' +
+    '</optgroup>' +
+    '</select>',
   props: {
-    value: {type: String, required: true, default: ''},
+    value: {required: true},
     options: {type: Object, required: true},
     placeholder: {type: String, default: '選擇一個選項...'},
-    class1: {type: Object, default: function(){ return {} } },
+    class1: {type: Object, default: function () { return {} }},
     debug: {type: Boolean, default: false},
+    multiple: {type: Boolean, default: false}
   },
-  mounted() {
-    var my = this;
-    $(this.$el).change(function(){
-      if(this.debug) console.log('chosen:change', this.value);
-      my.updateValue(this.value); // this == this.$el
-    });
-    this.init();
+  mounted () {
+    $(this.$el).change(this.emitValue)
+    this.init()
+  },
+  computed: {
+    optgroups () {
+      let result = {}
+      let key
+      for (key in this.options) {
+        if (this.isObjectLike(this.options[key])) {
+          result[key] = this.options[key]
+        }
+      }
+      return result
+    },
+    normalOptions () {
+      let result = {}
+      let key
+      for (key in this.options) {
+        if (this.isNotObject(this.options[key])) {
+          result[key] = this.options[key]
+        }
+      }
+      return result
+    }
   },
   methods: {
-    init() {
-      if(this.debug) console.log('chosen:init');
+    init () {
+      if (this.debug) console.log('chosen:init')
       $(this.$el).chosen({
         disable_search_threshold: 5,
         inherit_select_classes: true,
-        no_results_text: "什麼都沒找到...",
+        no_results_text: '什麼都沒找到...',
         search_contains: true,
-        width: '100%',
-      });
-      this.$nextTick(function(){
+        width: '100%'
+      })
+      this.$nextTick(function () {
         // 修正 select 在 value 為空時 inserted 後會自動選擇第一個的問題
-        this.$el.value = this.value;
-        $(this.$el).trigger('chosen:updated');
-      });
-      return this;
+        if (this.debug) console.log('reset', this.value)
+        $(this.$el).val(this.value).trigger('chosen:updated')
+      })
+      return this
     },
-    destroy() {
-      if(this.debug) console.log('chosen:destroy');
-      $(this.$el).chosen('destroy');
-      return this;
+    destroy () {
+      if (this.debug) console.log('chosen:destroy')
+      $(this.$el).chosen('destroy')
+      return this
     },
-    updateValue(value) {
-      this.$emit('input', value);
+    emitValue () {
+      let value = $(this.$el).val()
+      if (this.multiple && value == null) value = []
+      if (this.debug) console.log('emit', value)
+      this.$emit('input', value, 1)
     },
+    isObjectLike (value) {
+      return value != null && typeof value === 'object'
+    },
+    isNotObject (value) {
+      return value == null || typeof value !== 'object'
+    }
   },
   watch: {
-    value() {
-      this.$nextTick(function(){
-        if(this.debug) console.log('chosen:updated', this.value);
-        $(this.$el).trigger('chosen:updated');
-      });
+    value () {
+      this.$nextTick(function () {
+        if (this.debug) console.log('chosen:updated', this.value)
+        $(this.$el).val(this.value).trigger('chosen:updated')
+      })
     },
-    options() {
-      this.$nextTick(function(){
-        if(this.debug) console.log('chosen:rebuild', this.options.length);
-        this.destroy().init();
-      });
-    },
+    options () {
+      this.$nextTick(function () {
+        if (this.debug) console.log('chosen:rebuild', this.options.length)
+        this.destroy().init()
+      })
+    }
   },
-});
+  beforeDestroy () {
+    if (this.debug) console.log('chosen:beforeDestroy')
+    this.destroy()
+    $(this.$el).unbind('change', this.emitValue)
+  }
+})
